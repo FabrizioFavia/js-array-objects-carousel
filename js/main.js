53dc5f611289d57b7a13e581a007d75ce4abf183
FabrizioFavia <personal.favia@gmail.com>
2023-05-03 17:35:03 +0200

    feat(js): completed map.

--- js/main.js
+++ js/main.js
@@ -6,22 +6,22 @@ const images = [
         image: 'img/01.webp',
         title: 'Marvel\'s Spiderman Miles Morale',
         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-    }, 
+    },
     {
         image: 'img/02.webp',
         title: 'Ratchet & Clank: Rift Apart',
         text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-    }, 
+    },
     {
         image: 'img/03.webp',
         title: 'Fortnite',
         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-    }, 
+    },
     {
         image: 'img/04.webp',
         title: 'Stray',
         text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-    }, 
+    },
     {
         image: 'img/05.webp',
         title: "Marvel's Avengers",
@@ -34,11 +34,19 @@ images.map(element => {
     let image = document.createElement("img");
     image.src = `${path}${element.image}`;
     image.classList.add("image");
-     if (element!==images[0]) {
+
+    let title = document.createElement("p");
+    title.classList.add("imgTitle");
+    title.append(element.title);
+
+    let description = document.createElement("p");
+    description.classList.add("imgDscrpt");
+    description.append(element.text);
+
+    if (element !== images[0]) {
         image.classList.add("hidden");
-     };
-
-     slideContainer.append(image);
-});
-
-console.log(`${path}${element.image}`);
+        title.classList.add("hidden");
+        description.classList.add("hidden");
+    };
+    slideContainer.append(image, title, description);
+}); 
